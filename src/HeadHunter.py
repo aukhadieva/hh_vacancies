2cc65f012a202f2f716d1fb1d7f4f3c29dcf3382
Olga Aukhadieva <sarole4ka@gmail.com>
2024-02-13 07:21:47 +0500

    Fix get_vacancies method (HeadHunterAPI class)

--- src/HeadHunter.py
+++ src/HeadHunter.py
@@ -15,20 +15,21 @@ class HeadHunter(ABC):
 class HeadHunterAPI(HeadHunter):
     """Класс, наследующийся от абстрактного класса, для работы с платформой hh.ru."""
 
-    def get_vacancies(self, vacancy_name: str) -> dict:  # передается ключевое слово для поиска по вакансиям, н-р "Python"
+    def get_vacancies(self, key_word: str) -> dict:  # передается ключевое слово для поиска по вакансиям, н-р "Python"
         """Метод для подключения к API и получения вакансий."""
         # Получение вакансий с hh.ru в формате JSON
         params = {
-            'text': f'NAME:{vacancy_name}',  # Текст фильтра. В имени должно быть слово, н-р, "Python"
-            'area': 2,  # Поиск осуществляется по вакансиям города С-П
-            'page': 1,  # Индекс страницы поиска на HH
-            'per_page': 100  # Кол-во вакансий на 1 странице
+            'text': f'NAME:{key_word}',  # Текст фильтра. В имени должно быть слово, н-р, "Python"
+            'area': 1,  # Поиск осуществляется по вакансиям города по номеру
+            'page': 0,  # Индекс страницы поиска на HH
+            'per_page': 10  # Кол-во вакансий на 1 странице
         }
 
         req = requests.get('https://api.hh.ru/vacancies', params)  # Посылаем запрос к API
         data = req.content.decode()  # Декодируем его ответ, чтобы кириллица отображалась корректно
+        json_data = json.loads(data)
         with open('data_vacancy.json', 'w') as json_file:
-            json_file.write(data)
+            json_file.write(json.dumps(json_data, indent=4, ensure_ascii=False))
             with open('data_vacancy.json') as file:
                 vacancy_data = json.load(file)
                 return vacancy_data
