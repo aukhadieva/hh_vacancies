b843cc4c65db8189d848b5018b0d7607625840a0
Olga Aukhadieva <sarole4ka@gmail.com>
2024-02-13 20:05:34 +0500

    Fix add_vacancy and  delete_vacancy methods (JSONSaver class)

--- src/Saver.py
+++ src/Saver.py
@@ -3,8 +3,10 @@ from abc import ABC, abstractmethod
 
 
 class Saver(ABC):
-    """Абстрактный класс, который обязывает реализовать методы для добавления вакансий в файл,
-    получения данных из файла по указанным критериям и удаления информации о вакансиях."""
+    """
+    Абстрактный класс, который обязывает реализовать методы для добавления вакансий в файл,
+    получения данных из файла по указанным критериям и удаления информации о вакансиях.
+    """
 
     @abstractmethod
     def add_vacancy(self, vacancy):
@@ -25,16 +27,16 @@ class JSONSaver(Saver):
     def add_vacancy(self, vacancy):
         """Метод для добавления вакансий в файл."""
         with open('vacancy.json', 'w') as json_file:
-            json.dump(vacancy, json_file)
+            json.dump(vacancy, json_file, indent=4, ensure_ascii=False)
 
     def delete_vacancy(self, vacancy):
-        """Метод для удаления информации о вакансиях."""
+        """Метод для удаления информации о вакансиях по id вакансии."""
         with open('vacancy.json') as json_file:
             data = json.load(json_file)
-            if vacancy in data:
-                data.pop(vacancy)
+            if vacancy == data["items"][0]["id"]:
+                del data["items"][0]
                 with open('vacancy.json', 'w') as file:
-                    json.dump(data, file)
+                    json.dump(data, file, indent=4, ensure_ascii=False)
 
     def get_vacancy(self, vacancy):
         """Метод для получения данных из файла по указанным критериям."""
