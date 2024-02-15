c6c13f7cb2c308316866a06e79cfdaf495b3ac1e
Olga Aukhadieva <sarole4ka@gmail.com>
2024-02-15 18:07:55 +0500

    Fix filter_vacancies and get_vacancies_by_salary methods (Vacancy class)

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -17,7 +17,7 @@ class Vacancy:
     @classmethod
     def cast_to_object_list(cls, hh_vacancies):
         """Преобразование набора данных из JSON в список объектов."""
-        vacancies_list_ = []
+        vacancies_list = []
         for vacancy in hh_vacancies["items"]:
             vacancy_name = vacancy["name"]
             try:
@@ -37,21 +37,32 @@ class Vacancy:
                 employer = vacancy["employer"]["name"]
             except TypeError:
                 employer = None
-            vacancies_list_.append(cls(vacancy_name, salary, currency, requirement, responsibility, schedule, employer))
-        return vacancies_list_
+            vacancies_list.append(cls(vacancy_name, salary, currency, requirement, responsibility, schedule, employer))
+        return vacancies_list
 
     def __repr__(self):
+        """Используется для отладки информации об атрибутах класса Vacancy."""
         return (f'Вакансия: {self.vacancy_name}\nКомпания: {self.employer}\n'
                 f'Заработная плата: {self.salary} {self.currency}\n'
                 f'Требования: {self.requirement}\nОбязанности: {self.responsibility}\nГрафик: {self.schedule}\n')
 
+    @classmethod
     def filter_vacancies(self, vacancies_list, filter_words):
         """Метод для поиска вакансий по ключевому слову в файле."""
-        pass
+        filtered_vacancies = []
+        for item in vacancies_list:
+            if filter_words in item.vacancy_name:
+                filtered_vacancies.append(item)
+        return filtered_vacancies
 
+    @classmethod
     def get_vacancies_by_salary(cls, filtered_vacancies, salary_range):
         """Метод для поиска вакансий по зарплате в файле."""
-        pass
+        filtered_by_salary = []
+        for item in filtered_vacancies:
+            if salary_range == item.salary:
+                filtered_by_salary.append(item)
+        return filtered_by_salary
 
     def sort_vacancies(self, ranged_vacancies):
         """Метод для сравнения вакансий между собой по зарплате."""
