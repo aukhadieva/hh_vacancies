2817cdcc863813526e166cae86ebbf03d4c61c74
Olga Aukhadieva <sarole4ka@gmail.com>
2024-02-15 17:55:49 +0500

    Fix cast_to_object_list method (Vacancy class)

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -4,53 +4,59 @@ import json
 class Vacancy:
     """Класс для работы с вакансиями."""
 
-    def __init__(self, vacancy_name: str, employer: str, salary: int, requirement: str, responsibility: str, schedule: str) -> None:
+    def __init__(self, vacancy_name: str, salary: str, currency: str, requirement: str, responsibility: str,
+                 schedule: str, employer: str) -> None:
         self.vacancy_name = vacancy_name
-        self.employer = employer
         self.salary = salary
+        self.currency = currency
         self.requirement = requirement
         self.responsibility = responsibility
         self.schedule = schedule
+        self.employer = employer
 
     @classmethod
     def cast_to_object_list(cls, hh_vacancies):
         """Преобразование набора данных из JSON в список объектов."""
-        vacancy_name = hh_vacancies["items"][0]["name"]
-        try:
-            salary = hh_vacancies["items"][0]["salary"]["from"]
-        except TypeError:
-            salary = None
-        else:
-            salary = f'{hh_vacancies["items"][0]["salary"]["from"]} - {hh_vacancies["items"][0]["salary"]["to"]}'
-        requirement = hh_vacancies["items"][0]["snippet"]["requirement"]
-        responsibility = hh_vacancies["items"][0]["snippet"]["responsibility"]
-        schedule = hh_vacancies["items"][0]["schedule"]["name"]
-        try:
-            employer = hh_vacancies["items"][0]["employer"]["name"]
-        except TypeError:
-            employer = None
-        return cls(vacancy_name, salary, requirement, responsibility, schedule, employer)
+        vacancies_list_ = []
+        for vacancy in hh_vacancies["items"]:
+            vacancy_name = vacancy["name"]
+            try:
+                salary = vacancy["salary"]["from"]
+            except TypeError:
+                salary = None
+            else:
+                salary = f'{vacancy["salary"]["from"]}-{vacancy["salary"]["to"]}'
+            try:
+                currency = vacancy["salary"]["currency"]
+            except TypeError:
+                currency = None
+            requirement = vacancy["snippet"]["requirement"]
+            responsibility = vacancy["snippet"]["responsibility"]
+            schedule = vacancy["schedule"]["name"]
+            try:
+                employer = vacancy["employer"]["name"]
+            except TypeError:
+                employer = None
+            vacancies_list_.append(cls(vacancy_name, salary, currency, requirement, responsibility, schedule, employer))
+        return vacancies_list_
 
     def __repr__(self):
-        """Магический метод __repr__ для отладки информации о классе Vacancy."""
-        return (f'{self.__class__.__name__}({self.vacancy_name}, {self.employer}, {self.salary}, '
-                f'{self.requirement}, {self.responsibility}, {self.schedule})')
+        return (f'Вакансия: {self.vacancy_name}\nКомпания: {self.employer}\n'
+                f'Заработная плата: {self.salary} {self.currency}\n'
+                f'Требования: {self.requirement}\nОбязанности: {self.responsibility}\nГрафик: {self.schedule}\n')
 
     def filter_vacancies(self, vacancies_list, filter_words):
         """Метод для поиска вакансий по ключевому слову в файле."""
         pass
 
-    def get_vacancies_by_salary(self, filtered_vacancies,
-                                salary_range):  # self.filter_words = filter_words  # input("Введите ключевые слова для фильтрации вакансий: ").split()
+    def get_vacancies_by_salary(cls, filtered_vacancies, salary_range):
         """Метод для поиска вакансий по зарплате в файле."""
         pass
 
-    def sort_vacancies(self,
-                       ranged_vacancies):  # self.salary_range = salary_range  # input("Введите диапазон зарплат: ") # Пример: 100000 - 150000
+    def sort_vacancies(self, ranged_vacancies):
         """Метод для сравнения вакансий между собой по зарплате."""
         pass
 
-    def get_top_vacancies(self, sorted_vacancies,
-                          top_n):  # self.top_n = top_n  # int(input("Введите количество вакансий для вывода в топ N: "))
+    def get_top_vacancies(self, sorted_vacancies, top_n):
         """Метод для вывода топа вакансий."""
         pass
