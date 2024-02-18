488386c0a3b6668d75e2b25fc08376caf1026efa
Olga Aukhadieva <sarole4ka@gmail.com>
2024-02-18 17:46:38 +0500

    Fix user_interaction func

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+
+from config import ROOT
 from src.HeadHunter import HeadHunterAPI
 from src.Saver import JSONSaver
 from src.Vacancy import Vacancy
@@ -6,25 +9,24 @@ from src.Vacancy import Vacancy
 hh_api = HeadHunterAPI()
 
 # Получение вакансий с hh.ru в формате JSON
-hh_vacancies = hh_api.get_vacancies(input("Введите поисковый запрос: "))
-
-# Преобразование набора данных из JSON в список объектов
-vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
+hh_vacancies = hh_api.get_vacancies(input(f"Введите ключевые слова для фильтрации вакансий: \n"))
 
 # Сохранение информации о вакансиях в файл
 json_saver = JSONSaver()
 json_saver.add_vacancy(hh_vacancies)
-#json_saver.delete_vacancy(hh_vacancies)
+
+# Преобразование набора данных из JSON в список объектов
+vacancy_path = os.path.join(ROOT, 'data', 'vacancy.json')
+Vacancy.cast_to_object_list(vacancy_path)
+vacancies = Vacancy.vacancies_list  # все вакансии в виде списка экземпляров
 
 
 def user_interaction():
     """Функция для взаимодействия с пользователем."""
-    filter_words = input("Введите ключевые слова для фильтрации вакансий: ")#.split()
-    salary_range = input("Введите диапазон зарплат: ")
-    top_n = int(input(f"Введите количество вакансий для вывода в топ N: "))
+    salary_range = input(f"Введите диапазон зарплат или нажмите enter, чтобы увидеть все вакансии: \n")
+    top_n = input(f"Введите количество вакансий для вывода в топ N или нажмите enter, чтобы увидеть все вакансии: \n")
 
-    filtered_vacancies = Vacancy.filter_vacancies(vacancies_list, filter_words)
-    sorted_vacancies = Vacancy.sort_vacancies(filtered_vacancies)
+    sorted_vacancies = Vacancy.sort_vacancies(vacancies)
     filtered_by_salary = Vacancy.get_vacancies_by_salary(sorted_vacancies, salary_range)
     top_vacancies = Vacancy.get_top_vacancies(filtered_by_salary, top_n)
     Vacancy.print_vacancies(top_vacancies)
