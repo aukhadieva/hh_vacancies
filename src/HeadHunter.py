f6d1d5b4e1b17f667defa6212ad4df15a16b2f12
Olga Aukhadieva <sarole4ka@gmail.com>
2024-02-18 17:45:52 +0500

    Fix HeadHunter class

--- src/HeadHunter.py
+++ src/HeadHunter.py
@@ -3,14 +3,12 @@ from abc import ABC, abstractmethod
 
 import requests
 
-from src.Saver import JSONSaver
-
 
 class HeadHunter(ABC):
     """Абстрактный класс для работы с API сервиса с вакансиями."""
 
     @abstractmethod
-    def get_vacancies(self, vacancy_name):
+    def get_vacancies(self, key_word):
         pass
 
 
@@ -27,7 +25,7 @@ class HeadHunterAPI(HeadHunter):
             'text': f'{key_word}',  # Переданное знач-е ищется в полях в-сии, ук-х в пар-ре search_field(Область поиска)
             'area': (1, 2),  # Поиск осуществляется по вакансиям города по номеру
             'page': 0,  # Номер страницы (установлено дефолтное значение)
-            'per_page': 10  # Количество элементов на 1 странице (установлено дефолтное значение)
+            'per_page': 100  # Количество элементов на 1 странице (установлено дефолтное значение)
         }
 
         req = requests.get('https://api.hh.ru/vacancies', params)  # Посылаем запрос к API
