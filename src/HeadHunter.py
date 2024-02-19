d8d5111a393825b805b5bff8397b94711717295e
Olga Aukhadieva <sarole4ka@gmail.com>
2024-02-19 08:34:11 +0500

    Fix get_vacancies method (HeadHunter class)

--- src/HeadHunter.py
+++ src/HeadHunter.py
@@ -3,6 +3,8 @@ from abc import ABC, abstractmethod
 
 import requests
 
+from src.Saver import JSONSaver
+
 
 class HeadHunter(ABC):
     """Абстрактный класс для работы с API сервиса с вакансиями."""
@@ -19,16 +21,14 @@ class HeadHunterAPI(HeadHunter):
         """
         Метод для подключения к API и получения вакансий.
         Получает вакансии с hh.ru в формате JSON.
-        Возвращает объект Python.
         """
         params = {
             'text': f'{key_word}',  # Переданное знач-е ищется в полях в-сии, ук-х в пар-ре search_field(Область поиска)
             'area': (1, 2),  # Поиск осуществляется по вакансиям города по номеру
             'page': 0,  # Номер страницы (установлено дефолтное значение)
-            'per_page': 100  # Количество элементов на 1 странице (установлено дефолтное значение)
+            'per_page': 100  # Количество элементов на 1 странице
         }
 
-        req = requests.get('https://api.hh.ru/vacancies', params)  # Посылаем запрос к API
-        data = req.content.decode()  # Декодируем его ответ, чтобы кириллица отображалась корректно
-        json_data = json.loads(data)  # Преобразуем строку в формате JSON в объект Python
-        return json_data
+        request = requests.get('https://api.hh.ru/vacancies', params)  # Посылаем запрос к API
+        data = request.json()
+        return data
