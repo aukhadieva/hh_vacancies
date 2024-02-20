5e3867d22cd9023cfbb987222936e2180bd4acb2
Olga Aukhadieva <sarole4ka@gmail.com>
2024-02-20 11:44:04 +0500

    Added Vacancies class, fix Vacancy's methods

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -1,14 +1,100 @@
 import json
 
 
+class Vacancies:
+    """Класс для вакансий."""
+
+    def __init__(self, hh_vacancies) -> None:
+        self.__hh_vacancies = hh_vacancies
+
+    @property
+    def hh_vacancies(self) -> str:
+        """Геттер для приватного атрибута __hh_vacancies."""
+        return self.__hh_vacancies
+
+    def load_vacancies(self) -> dict:
+        """Загружает вакансии из json-словаря. Возвращает словарь."""
+        with open(self.hh_vacancies) as json_file:
+            loaded_vacancies = json.load(json_file)
+            return loaded_vacancies
+
+    def sort_salary(self, loaded_vacancies, salary_range) -> list:
+        """Сравнивает вакансии из json-словаря по зарплате. Возвращает отфильтрованный список."""
+        ranked_by_salary = []
+        for item in loaded_vacancies['items']:
+            try:
+                if item['salary'] is None:
+                    item['salary'] = 'Нет данных'
+                if item['salary']['from'] is None:
+                    item['salary']['from'] = 'Нет данных'
+                if item['salary']['to'] is None:
+                    item['salary']['to'] = 'Нет данных'
+                if (salary_range == str(item['salary']['from']) or salary_range == str(item['salary']['to'])
+                        or salary_range == f'{item["salary"]["from"]}-{item["salary"]["to"]}'):
+                    ranked_by_salary.append(item)
+            except TypeError:
+                pass
+            if salary_range == '':
+                ranked_by_salary.append(item)
+        return ranked_by_salary
+
+    @staticmethod
+    def sort_currency(ranked_by_salary) -> list:
+        """Сортирует вакансии по валюте. Возвращает отсортированный список."""
+        ranked_by_currency = []
+        for item in ranked_by_salary:
+            try:
+                if item['salary']['currency'] == "RUR":
+                    item['salary']['currency'] = "руб."
+                    ranked_by_currency.append(item)
+            except TypeError:
+                pass
+            if item['salary'] == 'Нет данных':
+                ranked_by_currency.append(item)
+        return ranked_by_currency
+
+    @staticmethod
+    def sort_req_res(ranked_by_currency) -> list:
+        """Сортирует вакансии по требованиям и обязанностям. Возвращает отсортированный список."""
+        ranked_by_other = []
+        for item in ranked_by_currency:
+            try:
+                item["snippet"]["requirement"] = item["snippet"]["requirement"].replace(
+                    '<highlighttext>Python</highlighttext>',
+                    'Python')
+            except AttributeError:
+                if item["snippet"]["requirement"] is None:
+                    item["snippet"]["requirement"] = 'Информация не указана'
+            try:
+                item["snippet"]["responsibility"] = item["snippet"]["responsibility"].replace(
+                    '<highlighttext>Python</highlighttext>',
+                    'Python')
+            except AttributeError:
+                if item["snippet"]["responsibility"] is None:
+                    item["snippet"]["responsibility"] = 'Информация не указана'
+            ranked_by_other.append(item)
+        return ranked_by_other
+
+    def sort_area(self, ranked_by_other, area) -> list:
+        """Сравнивает вакансии по городу. Возвращает отфильтрованный список."""
+        ranked_by_area = []
+        for item in ranked_by_other:
+            if area == item['area']['name']:
+                ranked_by_area.append(item)
+            elif area == '':
+                ranked_by_area.append(item)
+        return ranked_by_area
+
+
 class Vacancy:
-    """Класс для работы с вакансиями."""
+    """Класс для вакансии."""
 
     vacancies_list = []
 
-    def __init__(self, vacancy_name: str, employer: str, salary: str, currency: str, requirement: str, responsibility: str,
-                 schedule: str) -> None:
+    def __init__(self, vacancy_name: str, area: str, employer: str, salary: str, currency: str,
+                 requirement: str, responsibility: str, schedule: str):
         self.vacancy_name = vacancy_name
+        self.area = area
         self.employer = employer
         self.salary = salary
         self.currency = currency
@@ -19,89 +105,33 @@ class Vacancy:
         Vacancy.vacancies_list.append(self)
 
     @classmethod
-    def cast_to_object_list(cls, hh_vacancies) -> None:
-        """Преобразование набора данных из JSON в список объектов."""
-        with open(hh_vacancies) as json_file:
-            vacancy_data = json.load(json_file)
-            for vacancy in vacancy_data['items']:
-                vacancy_name = vacancy['name']
-                employer = vacancy["employer"]["name"]
-                try:
-                    if vacancy["salary"]["from"] is None:
-                        vacancy["salary"]["from"] = 'Нет данных'
-                    if vacancy["salary"]["to"] is None:
-                        vacancy["salary"]["to"] = 'Нет данных'
-                    salary = f'{vacancy["salary"]["from"]}-{vacancy["salary"]["to"]}'
-                except TypeError:
-                    salary = None
-                try:
-                    currency = vacancy["salary"]["currency"]
-                except TypeError:
-                    currency = 'Валюта не указана'
-                requirement = vacancy["snippet"]["requirement"]
-                responsibility = vacancy["snippet"]["responsibility"]
-                schedule = vacancy["schedule"]["name"]
-                cls(vacancy_name, employer, salary, currency, requirement, responsibility, schedule)
+    def cast_to_object_list(cls, vacancies) -> None:
+        """Преобразует набор данных из отфильтрованного списка в список объектов."""
+        for vacancy in vacancies:
+            vacancy_name = vacancy['name']
+            area = vacancy['area']['name']
+            employer = vacancy["employer"]["name"]
+            try:
+                salary = f'{vacancy["salary"]["from"]}-{vacancy["salary"]["to"]}'
+            except TypeError:
+                salary = vacancy["salary"]
+            try:
+                currency = vacancy["salary"]["currency"]
+            except TypeError:
+                currency = 'Валюта не указана'
+            requirement = vacancy["snippet"]["requirement"]
+            responsibility = vacancy["snippet"]["responsibility"]
+            schedule = vacancy["schedule"]["name"]
+            cls(vacancy_name, area, employer, salary, currency, requirement, responsibility, schedule)
 
-    def __repr__(self):
-        """
-        Возвращает текстовое представление объекта полезное для отладки
-        в виде названия классов и его атрибутов.
-        """
-        return (f'{self.__class__.__name__} ({self.vacancy_name}, {self.employer}, {self.salary}, {self.currency}, '
-                f'{self.requirement}, {self.responsibility}, {self.schedule})')
-
-    def __str__(self):
+    def __str__(self) -> str:
         """Возвращает строковое представление объекта."""
-        return (f'Вакансия: {self.vacancy_name}\nКомпания: {self.employer}\n'
+        return (f'Вакансия: {self.vacancy_name}\nГород: {self.area}\nКомпания: {self.employer}\n'
                 f'Заработная плата: {self.salary} {self.currency}\n'
                 f'Требования: {self.requirement}\nОбязанности: {self.responsibility}\nГрафик: {self.schedule}\n')
 
     @classmethod
-    def sort_vacancies(cls, vacancies) -> list:
-        """Метод для сравнения вакансий между собой."""
-        ranged_vacancies = []
-        for item in vacancies:
-            if item.salary is None:
-                item.salary = 'Зарплата не указана'
-            try:
-                item.requirement = item.requirement.replace('<highlighttext>Python</highlighttext>',
-                                                            'Python')
-            except AttributeError:
-                if item.requirement is None:
-                    item.requirement = 'Информация не указана'
-            try:
-                item.responsibility = item.responsibility.replace('<highlighttext>Python</highlighttext>',
-                                                                  'Python')
-            except AttributeError:
-                if item.responsibility is None:
-                    item.responsibility = 'Информация не указана'
-            ranged_vacancies.append(item)
-        return ranged_vacancies
-
-    @classmethod
-    def get_vacancies_by_salary(cls, ranged_vacancies, salary_range) -> list:
-        """Метод для поиска вакансий по зарплате в файле."""
-        filtered_by_salary = []
-        for item in ranged_vacancies:
-            try:
-                if salary_range in item.salary:
-                    filtered_by_salary.append(item)
-            except TypeError:
-                pass
-        return filtered_by_salary
-
-    @classmethod
-    def get_top_vacancies(cls, sorted_vacancies, top_n) -> list:
-        """Метод для вывода топа вакансий."""
-        try:
-            return sorted_vacancies[:int(top_n)]
-        except ValueError:
-            if top_n == '':
-                return sorted_vacancies
-
-    @classmethod
-    def print_vacancies(cls, top_vacancies) -> None:
-        """Выводит отобранную вакансию/ -и пользователю."""
-        for vacancy in top_vacancies:
+    def print_vacancies(cls, vacancies_list) -> None:
+        """Выводит отобранные вакансии пользователю."""
+        for vacancy in vacancies_list:
             print(vacancy)
